d6a7a9ae8dc7de76fbbba634c93fa49a19b80cef
oguzozkan <34229282+oguzozkan@users.noreply.github.com>
2021-09-04 13:29:41 +0300

    s

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          ggggggggggggggggggggggggggggggg
+          gggggggg232312323123213231231232121gggggggggggg
         </a>
       </header>
     </div>
