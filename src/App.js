41f744169bde111f3d9f024242982093f352ae3f
oguzozkan <34229282+oguzozkan@users.noreply.github.com>
2021-09-05 13:46:37 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import Home2 from "./pages/home2";
 import Deneme from "./Components/Deneme";
 import GalaryRew from "./Components/Galary/GalaryRew";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { HashRouter as Router, Route, Switch, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <Router>
         <Switch>
-          <Route exact path={"/portfolio"} component={Home} />
-          <Route path={"/portfolio/gallery"} component={GalaryRew} />
-          <Route path={"/portfolio/portfolios"} component={Home2} />
+          <Route path={"/portfolio"} exact component={Home} />
+          <Route path={"/portfolio/gallery"} exact component={GalaryRew} />
+          <Route path={"/portfolio/portfolios"} exact component={Home2} />
         </Switch>
       </Router>
     </div>
